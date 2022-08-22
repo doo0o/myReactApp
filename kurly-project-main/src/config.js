5f5d712518f02ed14ef0c1674f1bf3d177d9a13f
doo0o <dooo_o@kakao.com>
2022-08-22 16:28:15 +0900

    컬리마켓 삭제

--- kurly-project-main/src/config.js
+++
@@ -1,6 +0,0 @@
-export const API = "http://localhost:3001/";
-export const KURLY_API = "https://api.kurly.com/v2/categories?ver=1";
-export const MAIN_BANNER_API = "http://localhost:3001/main"
-export const MAIN_PRODUCT_API = "http://localhost:3002/products"
-export const RECIPE_API = "http://localhost:3003/recipe"
-export const RANDOM_BANNER_API = "http://localhost:3004/randombanner"
