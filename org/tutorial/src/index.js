7b2e41b9a477e738a3406597e1a84a40cc08d80f
doo0o <dooo_o@kakao.com>
2022-08-23 09:12:40 +0900

    리액트

---
+++ org/tutorial/src/index.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+
+class ShoppingList extends React.Component {
+    render() {
+        // return React.createElement('div', {className: 'shopping-list'},
+        React.createElement('h1', /* ... h1 children ... */),
+        React.createElement('ul', /* ... ul children ... */),
+        /*#__PURE__*/
+        /*#__PURE__*/
+    /*#__PURE__*/
+    /*#__PURE__*/
+React.createElement("div", {
+    className: "shopping-list"
+  }, /*#__PURE__*/React.createElement("h1", null, "Shopping List for ", props.name), /*#__PURE__*/React.createElement("ul", null, /*#__PURE__*/React.createElement("li", null, "Instagram"), /*#__PURE__*/React.createElement("li", null, "WhatsApp"), /*#__PURE__*/React.createElement("li", null, "Oculus")));
+    };
+  };
+  
