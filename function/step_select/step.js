0fbfa92a71ef48dc2064c84802363fffd6317e44
doo0o <dooo_o@kakao.com>
2022-11-08 08:41:20 +0900

    1101 ~ 1108 step, swiper 기능 고도화

--- function/step_select/step.js
+++ function/step_select/step.js
@@ -1,68 +1,127 @@
-function selectStep (){
-	const _body = document.querySelector('body');
-	const siblingItem = document.querySelectorAll(".question__item");
-	const radio = document.querySelectorAll('input[type="radio"]');
-	const textInput = document.querySelectorAll('.form_area input');
-	const question = document.querySelector('.question');
-	const chk = document.querySelector('input[type="radio"]').checked;
-	const itemLength = siblingItem.length;
+const selectStep = (callback) => {
+	window.onbeforeunload = () => window.scrollTo(0, 0)
+	
+	// Element
+	const wrap= document.getElementById('stepSelect');
+	const itemSibling= wrap.querySelectorAll(".question_item");
+	const nextButton= wrap.querySelectorAll(".nextButton");
+	const radioElement= wrap.querySelectorAll('input[type="radio"]');
+	const inputElement= wrap.querySelectorAll('[class^="inp_type"] input');
+	const textareaElement = wrap.querySelectorAll('textarea');
+
+	const scrollWrap = wrap.querySelector('.question');
+
+	let viewArea = null;
+	let _this = null;
+	let _target = null;
+	let _sibling = null;
+	let _button = null;
+	let idx = 0;
+	let scrolly = 0;
+	let x = 0;
+	let SwichObj = {
+		enter : false, 
+	};
+
+	wrap.addEventListener('wheel', preventScroll, {passive: false});
+
+	function radioIdCheck(keyWord, _this){ return _this.getAttribute("id").search(keyWord)}
 	
 
-	let item = null;
-	let num = 0;
-
-
-	_body.addEventListener('wheel', preventScroll, {passive: false});
-
-	function preventScroll(e){
+	radioElement.forEach(el => {el.addEventListener('click', clickOrTouch);})
+	nextButton.forEach(el => {el.addEventListener('click', nextBtnClick);})
+	inputElement.forEach(el => {el.addEventListener('keydown', keydown);})
+	textareaElement.forEach(el => {el.addEventListener('keydown', keydown);})
+	
+	function  preventScroll(e){
 		e.preventDefault();
-		e.stopPropagation();
-	
+		e.stopPropagation(); 
 		return false;
 	}
 
-	// _body.addEventListener('wheel',(e)=>{preventDefault(e);})
-	radio.forEach(el => {
-		el.addEventListener('click', radioChk);
-	})
-	
-	question.addEventListener('scroll', (e) => {
-		console.log(e.target.scrollTop)
-	})
+	function clickOrTouch(e){
+		viewArea = e.target.closest('.question_item').querySelector('.viewForm');
+		if(radioIdCheck('yes', e.target) == 0){
+			evtHeight = e.target.closest('.question_item').scrollHeight;
+			viewArea.setAttribute('data-view', 'true');
+		}else if(radioIdCheck('no', e.target) == 0){
+			viewArea.setAttribute('data-view', 'false');
+			evtHeight = e.target.closest('.question_item').scrollHeight;
+			if(viewArea.getAttribute('data-view') == 'false'){nextStep(e, evtHeight);}
+		}
+	}
 
-	textInput.forEach(el => {
-		el.addEventListener('keyup', (e) => {
-			if(e.keyCode === 13){
-				nextStep(e, item);
+	function nextBtnClick(e){
+		evtHeight = e.target.closest('.question_item').scrollHeight
+		if(e.type == 'click' && e.target.getAttribute('type') == 'button'){
+			nextStep(e, evtHeight);
+		}
+	}
+
+	function keydown(e){
+		evtHeight = e.target.closest('.question_item').scrollHeight
+		_this = e.target;
+		_button = e.target.closest('.question_item').querySelector('button');
+
+		if(e.keyCode !== 13){
+			SwichObj.enter = true;
+			if(_this.getAttribute('data-num')){
+				_this.value = numberWithCommas(chkNumber(_this.value))
 			}
-		});
-	})
-
-	function radioChk (e){
-		item = this.closest('.question__item');
-		const _input = item.querySelector('div[data-input]');
-
-		if(_input == null){
-			return nextStep(e, item)
 		}
-		if(this.getAttribute("id").search("yes") > -1 && this.checked == true){
-			_input.setAttribute('data-input', "true");
-			_input.closest('.form_area').classList.remove('focus');
-		}else{
-			_input.setAttribute('data-input', "false");
-			_input.closest('.form_area').classList.remove('focus');
-			nextStep(e, item);
+		
+		if( _this.value.length > 0){
+			_button.removeAttribute('disabled');
+			if(e.keyCode === 13 && SwichObj.enter){
+				if(_this.getAttribute('data-num')){
+					_this.value = numberWithCommas(chkNumber(_this.value))
+					nextStep(e, evtHeight);
+				}else {
+					nextStep(e, evtHeight);
+				}
+			}
+		}else if( _this.value.length == 0){
+			_button.setAttribute('disabled', "");
 		}
 	}
-
-	function nextStep(e, item){
-		++num;
-		item.classList.remove('active')
-		siblingItem[num].classList.add('active');
-		focus();
-	}
+	function nextStep (e, evtHeight){
+		++idx;
+		if(idx < itemSibling.length){
+			_this = e.target;
+			_button = _this.closest('.question_item').querySelector('button');
+			_button.setAttribute('disabled', "");
+			_this.setAttribute('disabled', "");
+			_target = _this.closest('.question_item');
+			_sibling = itemSibling[idx];
 	
-	function focus(){
-		siblingItem[num].scrollIntoView({block: 'center',behavior: 'smooth' })
+			_target.classList.remove('active');
+			_sibling.classList.add('active');
+			focus(e, evtHeight);
+		}else if(idx == itemSibling.length){
+			console.log("end")
+			init(callback);
+		}
+
+		SwichObj.enter = false;
+	}
+
+	function focus(e, evtHeight){
+		scrolly = evtHeight + 180;
+		x -= scrolly
+		scrollWrap.style.top = x + 'px'
+	}
+
+	function init (callBack){
+		if(callBack){
+			callBack(this);
+		}
+		viewArea = null;
+		_this = null;
+		_target = null;
+		_sibling = null;
+		_button = null;
+		idx = 0;
+
+		return false
 	}
 }
--- function/swiper/swiper.js
+++ function/swiper/swiper.js
@@ -1,52 +1,83 @@
-const swiperOptions = (target, type, option) => {
-	const wrap = document.querySelector(target);
-	const itemLeng = wrap.querySelectorAll('.swiper-slide').length;
-	if(type == 'type2' && itemLeng > 1) {
-		let next = document.createElement('div');
-		let prev = document.createElement('div');
-		next.classList.add('swiper-button-next');
-		prev.classList.add('swiper-button-prev');
-		wrap.append(next, prev);
-	}
-	const typeOptions = {
-		'type1' : {
-			slidesPerView: 1,
-			loop: true,
+/**
+ * getSwiperOption customOpt
+ * @param {Number} _slidesPerView view 갯수
+ * @param {Number} _spaceBetween 여백
+ * @param {Boolean} _loop 반복
+ * @param {Element} _pagination bullet
+ * @param {Element} _slidesPerView next / prev button
+ * @param {Object} addOption Add Swiper API
+*/
+function getSwiperOption(customOpt) {
+	const _container = document.querySelector(customOpt.target);
+	const slideLeng = _container.querySelectorAll('.swiper-slide').length;
+	let _loop = customOpt.loop;
+	let _slidesPerView = customOpt.slidesPerView > 1 ? customOpt.slidesPerView : 1;
+	let _pagination, _navigation, _spaceBetween, _settingObj;
 
-		},
-		'type2' : {
-			slidesPerView: 2,
-			spaceBetween: 20,
-			loop: true,
-		}
-	}
-	const page = {
-		pagination : {
-			el : (itemLeng > 1) ? wrap.querySelector('[class*="swiper-pagination"]') : null,
-			clickable : true,
-		}
-	}
-	const navi = {
-		navigation: {
-			nextEl: wrap.querySelector('.swiper-button-next'),
-			prevEl: wrap.querySelector('.swiper-button-prev'),
-		}
-	}
-	
-	let result = typeOptions[type];
 
-	if(itemLeng > 1) {
-		switch (type) {
-			case 'type1' :
-				result = {...result, ...page}
-			break;
-			case 'type2' :
-				result = {...result, ...page, ...navi}
-			break;
+	_settingObj = {
+		slidesPerView: _slidesPerView,
+		spaceBetween: _spaceBetween,
+		loop: _loop,
+		pagination: _pagination,
+		navigation: _navigation,
+	};
+
+	if (slideLeng > 1) {
+		if (customOpt.pagination && customOpt.hasOwnProperty('pagination')) {
+			_settingObj.pagination = {
+				el: "[class^=swiper-pagination0]",
+			};
+		} else {
+			delete _settingObj.pagination;
+		}
+
+		if (customOpt.navigation && customOpt.hasOwnProperty('navigation')) {
+			_settingObj.navigation = {
+				nextEl: ".swiper-button-next",
+				prevEl: ".swiper-button-prev"
+			};
+		} else {
+			delete _settingObj.navigation;
+		}
+
+		if (customOpt.hasOwnProperty('spaceBetween')) {
+			_settingObj.spaceBetween = customOpt.spaceBetween;
+		} else {
+			delete _settingObj.spaceBetween;
+		}
+
+		if (customOpt.hasOwnProperty('spaceBetween')) {
+			_settingObj.spaceBetween = customOpt.spaceBetween;
+		} else {
+			delete _settingObj.spaceBetween;
+		}
+
+		if (customOpt.hasOwnProperty('loop')) {
+			_settingObj.spaceBetween = customOpt.spaceBetween;
+		} else {
+			delete _settingObj.loop;
+		}
+		
+		if(customOpt.hasOwnProperty('slidesPerView')){
+			_settingObj.slidesPerView = customOpt.slidesPerView;
+		} else {
+			_settingObj.slidesPerView = 1;
+		}
+
+	} else {
+		delete _settingObj.navigation;
+		delete _settingObj.pagination;
+		delete _settingObj.spaceBetween;
+		delete _settingObj.loop;
+		// delete _settingObj.slidesPerView;
+	}
+
+	if (customOpt.hasOwnProperty('addOption')) {
+		for (let i = 0; i < Object.keys(customOpt.addOption).length; i++) {
+			_settingObj[Object.keys(customOpt.addOption)[i]] = Object.values(customOpt.addOption)[i];
 		}
 	}
-	if(option) {
-		result = {...result, ...option}
-	}
-	return result
+
+	return _settingObj;
 }
